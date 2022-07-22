2055154e8adebb6de4c150fcf0430e19ffcec452
AnAvgGuy <97361188+AnAvgGuy@users.noreply.github.com>
2022-07-22 21:07:45 +0100

    Create client.js

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 
-const ws = new WebSocket("wss://0.0.0.0:8080");
+const ws = new WebSocket("wss://0.0.0.0:8080",{transports: ['websocket']}
+);
 /*"ws://localhost:8081"*/
 /*"ws://192.168.1.94:8081/both"*/
 ws.addEventListener("open", () => {
